86d067c049ed00396ca1463b27268e16268e30ef
GSK FS <GSK_FS@Live.com>
2021-09-29 15:05:39 +0500

    Initial Commit

---
+++ loaderAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loaderAnimation
+//
+//  Created by GSK on 9/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
